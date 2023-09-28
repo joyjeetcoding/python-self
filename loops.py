2d11a25e6f8d831e405a934f3e4f6a26c783b6f1
Joyjeet <joyxbhp6@gmail.com>
2023-09-29 02:08:13 +0530

    Python added

---
+++ loops.py
@@ -0,0 +1,38 @@
+# while
+
+# i = 1
+
+# while i <= 5:
+#     print("Hello")
+#     i = i + 1
+
+# i = 5
+# j = 1
+
+# while i >= 1:
+#     print("Telusko ", end='')
+#     while j <= 4:
+#         print("Rocks ", end='')
+#         j = j + 1
+#     i = i-1
+
+
+
+# x = ['Joyjeet', 65, 2.5]
+x = "JOYJEET"
+
+# for i in x:
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(11, 21, 2):
+#     print(i)
+
+# for i in range(20, 11, -1):
+#     print(i)
+
+for i in range(1, 21):
+    if i % 5 == 0:
+        print(i)
